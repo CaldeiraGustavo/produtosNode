7f096b8fc1beb9dc0feafcf04f202d244652515a
CaldeiraGustavo <gusteitiom@hotmail.com>
2023-09-03 17:10:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,12 @@ const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/nodecal.vps.webdock.cloud/privkey.pem'),
     cert: fs.readFileSync('/etc/letsencrypt/live/nodecal.vps.webdock.cloud/fullchain.pem')
 }
-const port = 8000
+const port = 80
 
 const routes = require('./router.js')
 
 app.use(routes)
 
-https.createServer(options, app).listen(port)
+https.createServer(options, app).listen(port, () => {
+    console.log(`listening port ${port}`)
+})
