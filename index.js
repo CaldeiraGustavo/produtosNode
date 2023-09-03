145bc7f00b5637dd6cb2611508647ebf548baab2
CaldeiraGustavo <gusteitiom@hotmail.com>
2023-09-03 17:08:24 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const https = require(‘https’)
 const fs = require('fs')
 const options = {
     key: fs.readFileSync('/etc/letsencrypt/live/nodecal.vps.webdock.cloud/privkey.pem'),
