aa2cd3ee2d5bedc2f0d7a19f99b4aeb96e8f74a8
Gustavo Caldeira <gustavoc.lightbase@sebraemg.com.br>
2023-09-25 10:51:18 -0300

    adicionado cors

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const express = require('express');
 const app = express();
 const port = 80;
 const routes = require('./router.js');
+const cors = require('cors');
 
-
-app.use(routes)
+app.use(cors());
+app.use(routes);
 
 app.listen(port, () => {
-    console.log(`listening port ${port}`)
+    console.log(`listening port ${port}`);
 })
