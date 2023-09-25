69bae0e5fa33189411491631c6af500e0aae2ebd
Gustavo Caldeira <gustavoc.lightbase@sebraemg.com.br>
2023-09-25 16:09:52 -0300

    subindo https

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const app = express();
 const port = 80;
 const routes = require('./router.js');
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https');
+
+const privateKey  = fs.readFileSync('./certificates/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('./certificates/fullchain.pem', 'utf8');
 
 app.use(cors());
 app.use(routes);
 
-app.listen(port, () => {
-    console.log(`listening port ${port}`);
-})
+const credentials = {key: privateKey, cert: certificate};
+
+// your express configuration here
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(port);
